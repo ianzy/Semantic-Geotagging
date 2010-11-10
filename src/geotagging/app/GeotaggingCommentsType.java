0bb5fb307d7bcad6a930599e872b2f1ac7270d33
kanutavri <kanupriya.tavri@west.cmu.edu>
2010-11-10 05:34:40 +0530

    added UI changes -Kanupriya Tavri

--- src/geotagging/app/GeotaggingCommentsType.java
+++ src/geotagging/app/GeotaggingCommentsType.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class GeotaggingCommentsType extends Activity {
 	@Override
@@ -14,13 +17,44 @@ public class GeotaggingCommentsType extends Activity {
         
         setContentView(R.layout.comment_type);
         
-        Button btnPlane = (Button) findViewById(R.id.btnRequestForHelp);
-        btnPlane.setOnClickListener(new OnClickListener() {
+        final EditText edit_comment = (EditText) findViewById(R.id.edit_comment);
+        final RadioButton radio_request_for_help = (RadioButton) findViewById(R.id.radio_request_for_help);
+        final RadioButton radio_problem_report = (RadioButton) findViewById(R.id.radio_problem_report);
+        final RadioButton radio_situation_description = (RadioButton) findViewById(R.id.radio_situation_description);
+        final RadioButton radio_something_else = (RadioButton) findViewById(R.id.radio_something_else);
+        final Button submit_comment = (Button) findViewById(R.id.submit_comment);
+             
+        OnClickListener radio_listener = new OnClickListener() {
             public void onClick(View v) {
-            	Intent intent = new Intent();
-            	intent.setClassName("geotagging.app","geotagging.app.GeotaggingEnteringInformation");
-            	startActivity(intent);
+                // Perform action on clicks
+                submit_comment.setEnabled(true);                
+            }
+        };
+        radio_request_for_help.setOnClickListener(radio_listener);
+        radio_problem_report.setOnClickListener(radio_listener);
+        radio_situation_description.setOnClickListener(radio_listener);
+        radio_something_else.setOnClickListener(radio_listener);
+                
+        submit_comment.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	StringBuffer message = new StringBuffer();
+            	message.append("Submitting the comment\n\"" + edit_comment.getText() + "\"\nAs a\n");
+            	if(radio_request_for_help.isChecked()){
+            		message.append(radio_request_for_help.getText());
+            	}
+            	if(radio_problem_report.isChecked()){
+            		message.append(radio_problem_report.getText());
+            	}
+            	if(radio_situation_description.isChecked()){
+            		message.append(radio_situation_description.getText());
+            	}
+            	if(radio_something_else.isChecked()){
+            		message.append(radio_something_else.getText());
+            	}
+            	Toast.makeText(GeotaggingCommentsType.this, message, Toast.LENGTH_LONG).show();
             }
         });
+        
+        
 	}
 }
--- src/geotagging/app/GeotaggingEntityResponse.java
+++ src/geotagging/app/GeotaggingEntityResponse.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class GeotaggingEntityResponse extends Activity {
 
@@ -15,14 +18,54 @@ public class GeotaggingEntityResponse extends Activity {
         
         setContentView(R.layout.entity_response);
         
-        Button buttonProblemReport = (Button) findViewById(R.id.buttonResponse);
+        final EditText edit_response = (EditText) findViewById(R.id.edit_response);
+        final RadioButton radio_big_picture = (RadioButton) findViewById(R.id.radio_big_picture);
+        final RadioButton radio_details = (RadioButton) findViewById(R.id.radio_details);
+        final RadioButton radio_explanation = (RadioButton) findViewById(R.id.radio_explanation);
+        final RadioButton radio_advice = (RadioButton) findViewById(R.id.radio_advice);
+        final RadioButton radio_alternative = (RadioButton) findViewById(R.id.radio_alternative);
+        final RadioButton radio_example = (RadioButton) findViewById(R.id.radio_example);
+                
+        final Button buttonProblemReport = (Button) findViewById(R.id.submit_response);
         buttonProblemReport.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-            	Intent intent = new Intent();
-            	intent.setClassName("geotagging.app","geotagging.app.GeotaggingEnteringInformation");
-            	startActivity(intent);
+            	StringBuffer message = new StringBuffer();
+            	message.append("Submitting the response\n\"" + edit_response.getText() + "\"\nAs a\n");
+            	if(radio_big_picture.isChecked()){
+            		message.append(radio_big_picture.getText());
+            	}
+            	if(radio_details.isChecked()){
+            		message.append(radio_details.getText());
+            	}
+            	if(radio_explanation.isChecked()){
+            		message.append(radio_explanation.getText());
+            	}
+            	if(radio_advice.isChecked()){
+            		message.append(radio_advice.getText());
+            	}
+            	if(radio_alternative.isChecked()){
+            		message.append(radio_alternative.getText());
+            	}
+            	if(radio_example.isChecked()){
+            		message.append(radio_example.getText());
+            	}
+            	Toast.makeText(GeotaggingEntityResponse.this, message, Toast.LENGTH_LONG).show();
             }
         });
+        
+        OnClickListener radio_listener = new OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on clicks
+            	buttonProblemReport.setEnabled(true);                
+            }
+        };
+        
+        radio_big_picture.setOnClickListener(radio_listener);
+        radio_details.setOnClickListener(radio_listener);
+        radio_explanation.setOnClickListener(radio_listener);
+        radio_advice.setOnClickListener(radio_listener);
+        radio_alternative.setOnClickListener(radio_listener);
+        radio_example.setOnClickListener(radio_listener);
 	}
 	
 }
